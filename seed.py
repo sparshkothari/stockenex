dbdc4b6d8429c80d7c04224af464ee36d90266da
Sparsh Kothari <Sparsh@mac-c02jkmnldr54.attlocal.net>
2020-04-24 00:00:03 -0700

    adding colorMap s3 lookup

--- seed.py
+++ seed.py
@@ -1,15 +1,20 @@
-from alexandria.colorMap.ColorMap import ColorMap
 from mongoengine import *
 from models import Stock
 from smart_open import open
+import json
 
 def fetchStocks():
     connect('stockenex')
+    with open('s3://stockenex/colorMap.json', 'r') as c:
+    	colorMap = json.load(c)[1]
+
     for line in open('s3://stockenex/stockData.csv'):
         lineInfo = line.split(',', 2)
         symbol = lineInfo[0]
-        color = ColorMap().getColorHex(lineInfo[1].lower())
-        value = lineInfo[2]
+        color = lineInfo[1].lower()
+        if color in colorMap.keys():
+        	color = colorMap[color]
+    	value = lineInfo[2]
         if Stock.objects(Q(symbol=symbol)):
             i = Stock.objects(Q(symbol=symbol))
             i.delete()

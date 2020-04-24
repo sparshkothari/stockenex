e9c445f05492d2edcade8b7cc9f253e560ac06bc
Sparsh Kothari <Sparsh@mac-c02jkmnldr54.attlocal.net>
2020-04-23 23:12:07 -0700

    adding s3-lookup to seed.py/changing database name to stockenex

--- seed.py
+++ seed.py
@@ -1,21 +1,18 @@
 from alexandria.colorMap.ColorMap import ColorMap
 from mongoengine import *
 from models import Stock
-import os
-
+from smart_open import open
 
 def fetchStocks():
-    connect('pyUser')
-    fileName = os.getcwd() + "/assets/data/stockData.csv"
-    with open(fileName, "r+") as CSVFile:
-        for line in CSVFile.readlines():
-            lineInfo = line.split(',', 2)
-            symbol = lineInfo[0]
-            color = ColorMap().getColorHex(lineInfo[1].lower())
-            value = lineInfo[2]
-            if Stock.objects(Q(symbol=symbol)):
-                i = Stock.objects(Q(symbol=symbol))
-                i.delete()
-            s = Stock(symbol=symbol, value=value, color=color)
-            s.save()
+    connect('stockenex')
+    for line in open('s3://stockenex/stockData.csv'):
+        lineInfo = line.split(',', 2)
+        symbol = lineInfo[0]
+        color = ColorMap().getColorHex(lineInfo[1].lower())
+        value = lineInfo[2]
+        if Stock.objects(Q(symbol=symbol)):
+            i = Stock.objects(Q(symbol=symbol))
+            i.delete()
+        s = Stock(symbol=symbol, value=value, color=color)
+        s.save()
     disconnect()
